a6c6924029f3140a700e3185441e467baf5080a6
Eduardo Paes <eduardo-paes@outlook.com>
2022-01-13 08:34:41 -0300

    Alterando a landing page

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -4,9 +4,16 @@ import './styles.css';
 function MainContent() {
   return (
     <div className='main-content-container'>
-      <video className='video-player' autoPlay loop muted>
+      {/* <video className='video-player' autoPlay loop muted>
         <source src="/videos/video-1.mp4" type='video/mp4'/>
-      </video>
+      </video> */}
+
+      <img
+        className='video-player'
+        alt="landing"
+        src="/images/img(28).jpg" //25,28,30,31,36,37,38,43,47
+      />
+
       {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       {/* { VideoPlayer() } */}
       <h1 className='main-content-title'>{'Eduardo & Giovana'}</h1>
