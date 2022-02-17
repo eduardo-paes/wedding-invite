6fc604bd963e640c617fc81ad02d8edf56512c7e
Eduardo Paes <eduardo-paes@outlook.com>
2022-02-17 14:22:57 -0300

    Ajustes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,17 +12,20 @@ export default function Footer() {
       <p className='footer-question'>
         Gostaria de nos presentear?
       </p>
+
       <p className='footer-text'>
         É só acessar a lista de presentes abaixo ou deixar a sua 
-        <br />contribuição através do botão de PIX.
+        contribuição através do botão de PIX.
       </p>
-      <div className='input-areas'>
+
+      <div>
         <LinkButton text="PRESENTES" link="https://1drv.ms/x/s!ArFZQQ-pHyEdrDgST6ZPD4oDrA0x?e=msOGrb"/>
         <LinkButton text="PIX" link="https://nubank.com.br/pagar/xo385/2DPydkAAcr"/>
       </div>
-      <p className='footer-text'>
+
+      <p className='footer-thanks'>
         Desde já o nosso muito obrigado por fazer parte de 
-        <br />algum modo da nossa história!
+        algum modo da nossa história!
       </p>
     </div>
   );
