316aa5239641c24641d889dfd0df643063e798ab
Eduardo Paes <eduardo.paes@morningstar.com.br>
2022-04-09 10:35:53 -0300

    Ajuste na baseUrl

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({ 
-  // baseURL: 'https://eduardo-giovana.herokuapp.com/api/',
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://eduardo-giovana.herokuapp.com/api/',
+  // baseURL: 'http://localhost:5000/api/',
 });
 axiosInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
