a2c32556ae7e70dbaa6e43acba4b83c74591d1ef
eduardo-paes <eduardo-paes@outlook.com>
2022-10-19 10:53:54 -0300

    correção de imports

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Folheto, Login, Control, Convite } from "../pages";
+import { Login, Control, Convite } from "../pages";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/folheto" exact component={Folheto} />
+        {/* <Route path="/folheto" exact component={Folheto} /> */}
         <Route path="/login" exact component={Login} />
         <Route path="/control" exact component={Control} />
         <Route path="/" component={Convite} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Control from "./Control";
 import Convite from "./Convite";
 import Folheto from "./Folheto";
-import Home from "./Home";
 import Login from "./Login";
 
-export { Control, Convite, Folheto, Home, Login };
+export { Control, Convite, Folheto, Login };
